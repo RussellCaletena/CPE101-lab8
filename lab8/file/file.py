fed94fc657dd5483977f0cb7e1c6ef738c0eca2e
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 13:38:29 -0700

    Add Psuedocode to file.py

--- lab8/file/file.py
+++ lab8/file/file.py
@@ -4,4 +4,12 @@
 # Instructor: S. Einakian
 # Section: 5
 
-
+'''
+Pseudocode for lineDetails:
+Parameter(1): txtfile
+Open and read the file
+Get line number info
+Get number of characters in the line
+Get the text on each line
+return(1): Line {} ({} chars): {}.
+'''
