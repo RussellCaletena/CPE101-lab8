6307da6bfbea782d0f7b5ba71101487125e56e12
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 20:37:24 -0700

    Add functions distance and circles_overlap to funcs_objects.py

--- lab8/funcs_objects/funcs_objects.py
+++ lab8/funcs_objects/funcs_objects.py
@@ -1,5 +1,18 @@
 # Lab8
-# 
+#
 # Name: Russell Caletena
 # Instructor: S. Einakian
 # Section: 5
+
+def distance(argument1, argument2):
+    euclidDistance = 0
+    return euclidDistance
+    pass
+
+# Write this function using a relational operator and without using any sort of conditional
+def circles_overlap(argument1, argument2):
+    # Two circles will overlap when the distance between their center points is less than the sum of the radii
+    # if circles overlap:
+        #return True
+    # else:
+        #return False
