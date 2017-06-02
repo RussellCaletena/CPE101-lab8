3413738b91bccbfbbe9e2a94434d307bcdc0ccbb
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 19:36:31 -0700

    Create a class called Point with added __init__

--- lab8/funcs_objects/objects.py
+++ lab8/funcs_objects/objects.py
@@ -4,9 +4,11 @@
 # Instructor: S. Einakian
 # Section: 5
 
-class Point(self, x, y):
+class Point(object):
+    def __init__(self, newX, newY):
+        self.x = newX
+        self.y = newY
     pass
 
 class Circle(self, radius):
     pass
-    
