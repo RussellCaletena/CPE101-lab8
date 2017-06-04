0f48306a1528a9288535bbef1fe3093297bb85fd
RussellCaletena <RussellCaletena@gmail.com>
2017-06-03 18:00:35 -0700

    Add point in objects.py

--- lab8/funcs_objects/objects.py
+++ lab8/funcs_objects/objects.py
@@ -5,10 +5,14 @@
 # Section: 5
 
 class Point(object):
-    def __init__(self, newX, newY):
+    def __init__(self, newX = 0, newY = 0):
         self.x = newX
         self.y = newY
-    pass
+    
+P1 = Point()
+P1.x = 5
+P1.y = 10
+print (P1.x, ',', P1.y)
 
 class Circle(object):
     def __init__(self, center, radius):
