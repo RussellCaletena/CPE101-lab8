323b8d7f894e49014b12c0dfc85e3bf43394a21d
RussellCaletena <RussellCaletena@gmail.com>
2017-06-07 22:42:53 -0700

    Add list_comp.py

---
+++ lab8/list_comp/objects.py
@@ -0,0 +1,20 @@
+# Lab8
+#
+# Name: Russell Caletena
+# Instructor: S. Einakian
+# Section: 5
+# List_comp
+
+class Point(object):
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        checkEqual = ((self.x == other.x) and (self.y == other.y))
+        return checkEqual
+
+class Circle(object):
+    def __init__(self, center = Point(), radius = 0):
+        self.center = center
+        self.radius = radius
