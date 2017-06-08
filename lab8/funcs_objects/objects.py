00d6ff1d01e098b0b92bd7967d4e3aa25a8fdd98
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:24:29 -0700

    Comment objects.py

--- lab8/funcs_objects/objects.py
+++ lab8/funcs_objects/objects.py
@@ -4,12 +4,16 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# Define a class called Point that takes in an object as a parameter
 class Point(object):
+    # Define a function called __init__ with parameters self, x, and y to initialize attributes
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
 
+# Define a class called Circle that takes in an object as a parameter
 class Circle(object):
+    # Define a function called __init__ with parameters self, center, and radius to initialize attributes
     def __init__(self, center = Point(), radius = 0):
         self.center = center
         self.radius = radius
