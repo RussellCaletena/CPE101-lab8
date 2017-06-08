cc7734cf470d935996dedf782af17137bf7599dc
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 01:50:21 -0700

    Add distance_all and are_in_first_quadrant

--- lab8/list_comp/list_comp.py
+++ lab8/list_comp/list_comp.py
@@ -9,19 +9,8 @@ from objects import *
 from math import *
 
 
-listofPoints = [Point(1,0), Point(-5,4), Point(2,-7), Point(-9, -3)]
-'''
-for item in listofPoints:
-    print (x, y)
-'''
-#origin = (0,0)
-#distanceList = []
-# for item in listofPoints:
-    #distance = 0
-    #take x and y coordinates of origin and point and compute distance
-    #append distance to a list
-    #distanceList.append(distance)
-#return distanceList
+listofPoints = [Point(1,1), Point(-5,4), Point(2,-7), Point(-9, -3), Point(4,4)]
+print (listofPoints[0])
 
 def distance(P1):
     euclidDistance = sqrt(((P1.x)**2) + ((P1.y)**2))
@@ -29,7 +18,7 @@ def distance(P1):
 
 def distance_all(listOfPoints):
     distanceList = list(map(distance, listOfPoints))
-    print (distanceList)
+    #print (distanceList)
     #return distanceList
 
 '''
@@ -43,8 +32,20 @@ def distance_all([Point(1,0), Point(-5,4), Point(2,-7), Point(-9, -3)]):
     for item in list1:
         return list(map(sqrt(((item.x)**2) + ((item.y)**2)), list1))
 '''
-
-def are_in_first_quadrant():
-    pass
+def checkFirstQuadrant(P1):
+    if P1.x > 0 and P1.y > 0:
+        Point2 = ('({},{})'.format(P1.x, P1.y))
+        print (Point2)
+        return (Point2)
+        #return (P1.x, P1.y)
+        #print (Point(P1.x, P1.y))
+        #return P1.x, P1.y
+    else:
+        pass
+def are_in_first_quadrant(listOfPoints):
+    firstQuadrantList = list(filter(checkFirstQuadrant, listOfPoints))
+    #firstQuadrantList = list(filter(lambda P1: P1.x > 0 and P1.y > 0, listOfPoints))
+    print (firstQuadrantList)
 
 distance_all(listofPoints)
+are_in_first_quadrant(listofPoints)
--- lab8/list_comp/list_comp_tests.py
+++ lab8/list_comp/list_comp_tests.py
@@ -9,10 +9,12 @@ from list_comp import *
 from objects import *
 
 class TestCases(unittest.TestCase):
-   def test_1(self):
+   def test_distance_all(self):
+      self.assertEqual(distance_all)
+      pass
+   def test_are_in_first_quadrant(self):
       # Add code here.
       pass
-
 
 # Run the unit tests.
 if __name__ == '__main__':
