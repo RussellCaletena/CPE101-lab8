cf204bc17ac2b6e07637b1540d81f706e774fe04
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:49:32 -0700

    Comment utility.py

--- lab8/object_equality/utility.py
+++ lab8/object_equality/utility.py
@@ -4,5 +4,6 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# Define a function called epsilon_equal with parameters n, m, and epsilon and returns a boolean
 def epsilon_equal(n, m, epsilon=0.00001):
    return (n - epsilon) < m and (n + epsilon > m)
