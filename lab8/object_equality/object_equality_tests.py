e29e58f0de9a5fc6f2f472cec9ec0674015eb579
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:40:54 -0700

    Comment object_equality_tests.py

--- lab8/object_equality/object_equality_tests.py
+++ lab8/object_equality/object_equality_tests.py
@@ -2,10 +2,13 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# Import libraries
 import unittest
 from objects import *
 
+# Define a class called TestCases with parameter unittest.TestCase
 class TestCases(unittest.TestCase):
+    # Define a function called test___eq__ that tests the __eq__ function
     def test___eq__(self):
         aPoint = Point(5,5)
         thePoint = Point(5,5)
