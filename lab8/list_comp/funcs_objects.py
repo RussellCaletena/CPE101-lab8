99569945f728dc9ba97da4d7eb2a329273a5e208
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:26:52 -0700

    Comment funcs_objects.py

--- lab8/list_comp/funcs_objects.py
+++ lab8/list_comp/funcs_objects.py
@@ -4,13 +4,16 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# Import libraries
 from math import *
 from objects import *
 
+# Define a function called distance with parameters P1 and P2 and returns the distance between the two points
 def distance(P1, P2):
     euclidDistance = sqrt(((P2.x-P1.x)**2) + ((P2.y-P1.y)**2))
     return euclidDistance
 
+# Define a function called circles_overlap with parameters Circle1 and Circle2 and returns a boolean if the circles overlap
 def circles_overlap(Circle1, Circle2):
     sumRadii = Circle1.radius + Circle2.radius
     euclidDistance = sqrt(((Circle2.center.x-Circle1.center.x)**2) + ((Circle2.center.y-Circle1.center.y)**2))
