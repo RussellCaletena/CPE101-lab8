9bfe88ffe46877f9983897405971f2a493686f7b
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:31:58 -0700

    Comment list_comp.py

--- lab8/list_comp/list_comp.py
+++ lab8/list_comp/list_comp.py
@@ -4,19 +4,21 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# Import libraries
 from funcs_objects import *
 from objects import *
 from math import *
 
-
+# Create two lists containing points
 listofPoints = [Point(1,1), Point(-5,4), Point(2,-7), Point(-9, -3), Point(4,4)]
 listofPoints2 = [Point(3,-2), Point(5, 1), Point(-4, -6), Point(-7,7)]
 
-
+# Define a function called distance that takes in P1 as a parameter and returns the distance
 def distance(P1):
     euclidDistance = sqrt(((P1.x)**2) + ((P1.y)**2))
     return euclidDistance
 
+# Define a function called distance_all that takes in a list of Points as a paramter and returns a list of the distance from each point to the origin
 def distance_all(listOfPoints):
     distanceList = list(map(distance, listOfPoints))
     for i in range(len(distanceList)):
@@ -24,18 +26,14 @@ def distance_all(listOfPoints):
     print (distanceList)
     return distanceList
 
+# Define a function called checkFirstQuadrant that takes in P1 as a parameter and returns the point if it is in the 1st quadrant
 def checkFirstQuadrant(P1):
     if P1.x > 0 and P1.y > 0:
-        #P1.x *= 1
-        #P1.y *= 1
-        #Point2 = ('({},{})'.format(P1.x, P1.y))
-        #print (P1)
         return (P1)
-        #return (P1.x, P1.y)
-        #print (Point(P1.x, P1.y))
-        #print (P1.x, P1.y)
     else:
         pass
+
+# Define a function called are_in_first_quadrant that takes in a list of Points as a parameter and returns a list of points that are in the 1st quadrant
 def are_in_first_quadrant(listOfPoints):
     firstQuadrantList = list(filter(checkFirstQuadrant, listOfPoints))
     for i in range(len(firstQuadrantList)):
@@ -43,5 +41,6 @@ def are_in_first_quadrant(listOfPoints):
     print (firstQuadrantList)
     return firstQuadrantList
 
-distance_all(listofPoints2)
+# Test functions to run
+#distance_all(listofPoints2)
 #are_in_first_quadrant(listofPoints)
