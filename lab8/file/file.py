d24287d97adac72409b7e9fd69f38708fb2f97d6
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 13:13:40 -0700

    Comment file.py

--- lab8/file/file.py
+++ lab8/file/file.py
@@ -14,6 +14,7 @@ Get the text on each line
 return(1): Line {} ({} chars): {}.
 '''
 
+# Define a function called lineDetails that takes in a fileName as a parameter
 def lineDetails(fileName):
     fin = open(fileName)
     lineNumber = -1
@@ -22,5 +23,6 @@ def lineDetails(fileName):
         textLength = len(text)-1
         lineNumber += 1
         print ('Line {} ({} chars): {}'.format(lineNumber, textLength, text))
-        
+
+# Call the lineDetails function with parameter of in.txt
 lineDetails('in.txt')
