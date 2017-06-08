febb07cf0ff6da1b3223bffa34a9fb0557d9da34
RussellCaletena <RussellCaletena@gmail.com>
2017-06-07 22:37:52 -0700

    Test __eq__ function

--- lab8/object_equality/object_equality_tests.py
+++ lab8/object_equality/object_equality_tests.py
@@ -6,7 +6,7 @@ import unittest
 from objects import *
 
 class TestCases(unittest.TestCase):
-    def test_Point(self):
+    def test___eq__(self):
         aPoint = Point(5,5)
         thePoint = Point(5,5)
         self.assertEqual(aPoint, thePoint)
@@ -14,11 +14,6 @@ class TestCases(unittest.TestCase):
         anotherPoint = Point(-2,2)
         theOnePoint = Point()
         self.assertNotEqual(anotherPoint, theOnePoint)
-
-    def test_equality(self):
-        # Add test here
-        pass
-
 
 # Run the unit tests.
 if __name__ == '__main__':
--- lab8/object_equality/objects.py
+++ lab8/object_equality/objects.py
@@ -9,6 +9,7 @@ class Point(object):
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+
     def __eq__(self, other):
         checkEqual = ((self.x == other.x) and (self.y == other.y))
         return checkEqual
